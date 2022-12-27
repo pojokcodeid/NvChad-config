af092fb7fa02395f57ad22be56650a86c5286415
asep komarudin <pojokcodeid@gmail.com>
2022-12-27 15:21:42 +0700

    update

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,7 +10,8 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  -- b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -19,7 +20,25 @@ local sources = {
   b.formatting.clang_format,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    --if client.resolved_capabilities.document_formatting then
+    --vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format{async=true}")
+    --end
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+          -- vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }
