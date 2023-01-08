4543934013bf9b63959e085376827c7b7c492365
pojok code <pojokcodeid@gmail.com>
2023-01-08 15:53:43 +0700

    update winbar

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -1,9 +1,11 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+-- local on_attach = require("plugins.configs.lspconfig").on_attach
+-- local capabilities = require("plugins.configs.lspconfig").capabilities
+local on_attach = require("custom.lsp.handlers").on_attach
+local capabilities = require("custom.lsp.handlers").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "clangd", "emmet_ls", "jdtls", "intelephense", "pyright" }
+local servers = { "sumneko_lua", "html", "cssls", "tsserver", "clangd", "emmet_ls", "jdtls", "intelephense", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
