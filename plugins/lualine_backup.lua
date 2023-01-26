40724124c0f68676221cbfe539dac2aff9de13b9
asep komarudin <pojokcodeid@gmail.com>
2023-01-26 07:00:07 +0700

    update null-ls info on lualine

--- plugins/lualine_backup.lua
+++ plugins/lualine_backup.lua
@@ -25,7 +25,6 @@ local diagnostics = {
   "diagnostics",
   sources = { "nvim_diagnostic" },
   sections = { "error", "warn" },
-  -- symbols = { error = " ", warn = " " },
   symbols = {
     error = icons.diagnostics.BoldError .. " ",
     warn = icons.diagnostics.BoldWarning .. " ",
@@ -38,7 +37,6 @@ local diagnostics = {
 local diff = {
   "diff",
   colored = true,
-  -- symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
   symbols = {
     added = icons.git.LineAdded .. " ",
     modified = icons.git.LineModified .. " ",
@@ -83,7 +81,6 @@ local progress = function()
 end
 
 local spaces = function()
-  -- return "->| " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
   return icons.ui.Tab .. " " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
@@ -92,80 +89,30 @@ local file_name = {
   cond = conditions.buffer_not_empty,
 }
 
--- start for lsp
-local list_registered_providers_names = function(filetype)
-  local s = require "null-ls.sources"
-  local available_sources = s.get_available(filetype)
-  local registered = {}
-  for _, source in ipairs(available_sources) do
-    for method in pairs(source.methods) do
-      registered[method] = registered[method] or {}
-      table.insert(registered[method], source.name)
-    end
-  end
-  return registered
-end
-
-local null_ls = require "null-ls"
--- for formatter
-local list_registered = function(filetype)
-  local method = null_ls.methods.FORMATTING
-  local registered_providers = list_registered_providers_names(filetype)
-  return registered_providers[method] or {}
-end
-
---- for linter
-local alternative_methods = {
-  null_ls.methods.DIAGNOSTICS,
-  null_ls.methods.DIAGNOSTICS_ON_OPEN,
-  null_ls.methods.DIAGNOSTICS_ON_SAVE,
-}
-
-local linter_list_registered = function(filetype)
-  local registered_providers = list_registered_providers_names(filetype)
-  local providers_for_methods = vim.tbl_flatten(vim.tbl_map(function(m)
-    return registered_providers[m] or {}
-  end, alternative_methods))
-
-  return providers_for_methods
-end
--- end for lsp
-
 local lsp_info = {
   function()
-    --local msg = "No Active Lsp"
     local msg = "LS Inactive"
-    -- local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local buf_ft = vim.bo.filetype
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
     local clients = vim.lsp.get_active_clients()
-    -- start register
-    local buf_clients = vim.lsp.buf_get_clients()
-    local buf_client_names = {}
-    if next(buf_clients) == nil then
-      -- TODO: clean up this if statement
-      if type(msg) == "boolean" or #msg == 0 then
-        return "LS Inactive"
+    if next(clients) == nil then
+      for i, name in ipairs(clients) do
+        msg = msg .. " " .. name
       end
       return msg
     end
-    -- add client
-    for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" and client.name ~= "copilot" then
-        table.insert(buf_client_names, client.name)
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        if msg == "LS Inactive" then
+          msg = ""
+          msg = msg .. client.name
+        else
+          msg = msg .. ", " .. client.name
+        end
       end
     end
-    -- add formatter
-    local supported_formatters = list_registered(buf_ft)
-    vim.list_extend(buf_client_names, supported_formatters)
-    -- add linter
-    local supported_linters = linter_list_registered(buf_ft)
-    vim.list_extend(buf_client_names, supported_linters)
-    -- decomple
-    local unique_client_names = vim.fn.uniq(buf_client_names)
-    local msg = table.concat(unique_client_names, ", ")
     return msg
   end,
-  --icon = " ",
   icon = icons.ui.Gear .. "",
 }
 
@@ -173,28 +120,16 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = "auto",
+    -- theme = "tokyonight",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      "TelescopePrompt",
-      "packer",
-      "alpha",
-      "dashboard",
-      "NvimTree",
-      "Outline",
-      "DressingInput",
-      "toggleterm",
-      "lazy",
-      "mason",
-    },
+    disabled_filetypes = { "packer", "alpha", "dashboard", "Outline", "DressingInput", "toggleterm" },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = { branch },
     lualine_b = { mode },
     lualine_c = { diagnostics, lsp_info },
-    -- lualine_c = { file_name, lsp_info },
-    -- lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_x = { diff, spaces, "encoding", filetype },
     lualine_y = { location },
     lualine_z = { progress },
