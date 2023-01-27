ed62d7dbb94f6291995aed9df135f266a25a886c
asep komarudin <pojokcodeid@gmail.com>
2023-01-27 19:27:42 +0700

    update

--- plugins/init.lua
+++ plugins/init.lua
@@ -175,8 +175,10 @@ return {
 
   -- Additional View plugins
   ["rcarriga/nvim-notify"] = {
+    event = "BufWinEnter",
     config = function()
       vim.notify = require "notify"
+      -- vim.notify = require("notify").setup { background_colour = "#000000" }
     end,
   },
   -- untuk split
